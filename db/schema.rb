39dbd0235dc54c497bb86c6fd590b37096c17960
dattrinh09 <tiendattrinh09@gmail.com>
2021-12-15 03:49:37 +0000

    Add User Avatar

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-add_subjects
-ActiveRecord::Schema.define(version: 2021_12_14_165512) do
+ActiveRecord::Schema.define(version: 2021_12_15_031623) do
 
-ActiveRecord::Schema.define(version: 2021_12_14_151734) do
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
 
-  create_table "teachers", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.string "email"
-    t.string "subject"
-
-ActiveRecord::Schema.define(version: 2021_12_14_145650) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.integer "age"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-ActiveRecord::Schema.define(version: 2021_12_14_150315) do
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "subject_reviews", force: :cascade do |t|
     t.integer "user_id"
@@ -57,6 +51,15 @@ ActiveRecord::Schema.define(version: 2021_12_14_150315) do
     t.integer "user_id"
   end
 
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "email"
+    t.string "subject"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -71,4 +74,6 @@ ActiveRecord::Schema.define(version: 2021_12_14_150315) do
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
