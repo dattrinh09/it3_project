78d4df2157f90864600e3536fb70a5410d1328ce
doducthuan <doducthuan29@gmail.com>
2021-12-15 15:30:17 +0000

    fix logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  resources :discusses
   resources :teachers
   get 'users/show'
   devise_for :users
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   resources :subject_reviews
   resources :subjects
-  root to: "static_pages#home"
+  #root to: "static_pages#home"
+  root 'subjects#index'
   get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
