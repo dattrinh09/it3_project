715a1af6072e082ff36f30e06bdadd736c5254a2
doducthuan <doducthuan29@gmail.com>
2021-12-15 03:35:10 +0000

    add bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,34 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-add_subjects
 ActiveRecord::Schema.define(version: 2021_12_14_165512) do
-
-ActiveRecord::Schema.define(version: 2021_12_14_151734) do
-
-  create_table "teachers", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.string "email"
-    t.string "subject"
-
-ActiveRecord::Schema.define(version: 2021_12_14_145650) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.integer "age"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-ActiveRecord::Schema.define(version: 2021_12_14_150315) do
 
   create_table "subject_reviews", force: :cascade do |t|
     t.integer "user_id"
@@ -57,6 +30,15 @@ ActiveRecord::Schema.define(version: 2021_12_14_150315) do
     t.integer "user_id"
   end
 
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "email"
+    t.string "subject"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -71,4 +53,5 @@ ActiveRecord::Schema.define(version: 2021_12_14_150315) do
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
