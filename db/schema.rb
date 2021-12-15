1b5fe21634726800f6badb9e6aa5d12501e9543a
doducthuan <doducthuan29@gmail.com>
2021-12-15 11:09:12 +0000

    edit style

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_12_15_031623) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -33,8 +32,6 @@ ActiveRecord::Schema.define(version: 2021_12_15_031623) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-
-ActiveRecord::Schema.define(version: 2021_12_14_165512) do
 
   create_table "subject_reviews", force: :cascade do |t|
     t.integer "user_id"
@@ -77,7 +74,6 @@ ActiveRecord::Schema.define(version: 2021_12_14_165512) do
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
