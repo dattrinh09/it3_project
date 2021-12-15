177bf8e1c4ea2f53af1189f48226b0f3287a5650
phatdat0900 <dat.lt184060@sis.hust.edu.vn>
2021-12-15 05:41:28 +0000

    fix_teacher

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_12_15_031623) do
+ActiveRecord::Schema.define(version: 2021_12_15_043315) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -33,8 +32,6 @@ ActiveRecord::Schema.define(version: 2021_12_15_031623) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-
-ActiveRecord::Schema.define(version: 2021_12_14_165512) do
 
   create_table "subject_reviews", force: :cascade do |t|
     t.integer "user_id"
@@ -58,9 +55,9 @@ ActiveRecord::Schema.define(version: 2021_12_14_165512) do
     t.string "name"
     t.integer "age"
     t.string "email"
-    t.string "subject"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "subject_code"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,7 +74,6 @@ ActiveRecord::Schema.define(version: 2021_12_14_165512) do
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
